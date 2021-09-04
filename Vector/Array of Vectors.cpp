d91137d91d97538557d8ef6116fa9a5430ad0acf
Bornali Saha <68811059+BornaliSaha@users.noreply.github.com>
2021-09-04 19:15:43 +0600

    Nesting of Vector

---
+++ Vector/Array of Vectors.cpp
@@ -0,0 +1,38 @@
+/*
+3
+4
+1 4 2 6
+3
+9 4 2
+2
+1 2
+*/
+
+void printVec(vector<int>&v)
+{
+    cout<<"size: "<<v.size()<<endl;
+    for(int i=0;i<v.size();i++)
+    {
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+}
+int main()
+{
+    int N;
+    cin>>N;
+    vector<int>v[N];
+    for(int i=0;i<N;i++)
+    {
+        int n;
+        cin>>n;
+        for(int j=0;j<n;j++)
+        {
+            int x;
+            cin>>x;
+            v[i].push_back(x);
+        }
+    }
+    for(int i=0;i<N;i++)
+        printVec(v[i]);
+}
