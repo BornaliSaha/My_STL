6dabbce75110deb78f65ad6afc8fef44bb0d8e71
Bornali Saha <bornalisaha.duet@gmail.com>
2021-09-08 21:33:31 +0600

    Add files via upload

---
+++ Set/ICPC.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+/**
+Given N strings, print unique strings in lexiographical order
+N<= 10^5
+|S| <= 100000
+**/
+
+void print(set<string> &st)
+{
+    for(string value: st){
+        cout<<value<<" ";
+    }
+}
+int main()
+{
+    set<string>st;
+    int n;
+    cin>>n;
+    string s;
+    for(int i=0;i<n;i++)
+    {
+        cin>>s;
+        st.insert(s);
+    }
+    print(st);
+}
